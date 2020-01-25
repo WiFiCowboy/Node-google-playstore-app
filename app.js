597513e6d6e5487afb77762826d0352725d7a532
Humberto Hernandez <hamburger45@gmail.com>
2020-01-25 15:19:15 -0800

    added genres stuff

--- app.js
+++ app.js
@@ -8,20 +8,50 @@ const store = require("./playstore.js");
 // default get should display
 // query parameters = sort and geners
 app.get("/apps", (req, res) => {
-  const { rating = "", genres } = req.query;
-  if (!rating) {
-    return res.status(400).send("Please enter rating");
+  const {
+    rating = "",
+    genres = ""
+  } = req.query;
+  
+  
+  // need logic to check if one or the orther is present!!
+  // if (!rating) {
+  //   return res.status(400).send("Please enter rating");
+  // }
+  // // this return regardless needs logic
+  // let results = store.filter(rate => rate.Rating);
+  // // need to check for value of rating
+  // if (rating) {
+  //   results.sort((a, b) => {
+  //     return a.Rating > b.Rating ? 1 : a.Rating < b.Rating ? -1 : 0;
+  //   });
+  // }
+
+
+  
+  
+
+  let includesGenres = [
+    "Action",
+    "Puzzle",
+    "Strategy",
+    "Casual",
+    "Arcade",
+    "Card"
+  ].includes(genres);
+  
+
+  if (!includesGenres) {
+    return res.status(400).send("Please enter a genre!");
   }
-  // this return regardless needs logic
-  let results = store.filter(rate => rate.Rating);
-  // need to check for value of rating
-  if (rating) {
-    results.sort((a, b) => {
-      return a.Rating > b.Rating ? 1 : a.Rating < b.Rating ? -1 : 0;
-    });
-  }
+
+  let storeGenres = store.filter(genre => genre.Genres.includes(genres));
+  console.log(storeGenres);
+  
+  
+
   // default return
-  res.json(results);
+  res.json(storeGenres);
 });
 
 app.listen(8000, () => {
